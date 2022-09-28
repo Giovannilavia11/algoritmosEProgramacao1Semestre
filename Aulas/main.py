1c5f6ad1e4ebfc1c7f8828cb8274994ec2b33fda
Giovanni Lavia <giovannilavia123@gmail.com>
2022-09-28 17:52:24 -0300

    Segundo commit

---
+++ Aulas/main.py
@@ -0,0 +1,10 @@
+n1 = int(input('Informe o valor de n1: '))
+n2 = int(input('Infome o valor de n2: '))
+
+print('Os valores são n1:{} e n2:{}'.format(n1,n2))
+
+temp = n1
+n1 = n2
+n2 = temp
+
+print('Os valores são n1:{} e n2:{}'.format(n1,n2))
